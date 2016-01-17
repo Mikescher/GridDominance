734d70b2be23245079bb86ceb4da2c28d7823a47
Mike Schwörer <mailport@mikescher.de>
2016-01-17 17:03:51 +0100

    Base Template

---
+++ GridDominance.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using GridDominance.Shared;
+
+#if OUYA
+using Ouya.Console.Api;
+#endif
+
+using Microsoft.Xna.Framework;
+
+namespace GridDominance.Android
+{
+	[Activity(Label = "GridDominance.Android", 
+		MainLauncher = true,
+		Icon = "@drawable/icon",
+		Theme = "@style/Theme.Splash",
+		AlwaysRetainTaskState = true,
+		LaunchMode = LaunchMode.SingleInstance,
+		ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.Keyboard | ConfigChanges.ScreenSize)]
+	#if OUYA
+	[IntentFilter(new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLauncher, OuyaIntent.CategoryGame })]
+	#endif
+	public class MainActivity : AndroidGameActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			var g = new MainGame();
+			SetContentView(g.Services.GetService<View>());
+			g.Run();
+		}
+		
+	}
+}
+
+
---
+++ GridDominance.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#if __ANDROID__
+using Android.App;
+#endif
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("GridDominance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SAM Software Development")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("SAM Software Development")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
