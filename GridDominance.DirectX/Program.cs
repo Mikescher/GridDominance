32ff720b48063732d113a29f0b097bedc8b14fd9
Mike Schwörer <mailport@mikescher.de>
2016-01-23 22:55:55 +0100

    Added OpenGL Project

--- GridDominance.DirectX/Program.cs
+++ GridDominance.DirectX/Program.cs
@@ -16,71 +16,14 @@ using MonoTouch.UIKit;
 
 namespace GridDominance.DirectX
 {
-	#if __IOS__
-	[Register("AppDelegate")]
-	class Program : UIApplicationDelegate
-	
-#else
 	static class Program
-	#endif
-    {
-		private static MainGame game;
-
-		internal static void RunGame()
-		{
-			game = new MainGame();
-			game.Run();
-		}
-
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		#if !MONOMAC && !__IOS__		 
-        [STAThread]
-		#endif
-		static void Main(string[] args)
-		{
-			#if MONOMAC
-			NSApplication.Init ();
-
-			using (var p = new NSAutoreleasePool ()) {
-				NSApplication.SharedApplication.Delegate = new AppDelegate();
-				NSApplication.Main(args);
-			}
-			#elif __IOS__
-			UIApplication.Main(args, null, "AppDelegate");
-			#else
-			RunGame();
-			#endif
-		}
-
-		#if __IOS__
-		public override void FinishedLaunching(UIApplication app)
-		{
-			RunGame();
-		}
-		#endif
-	}
-
-	#if MONOMAC
-	class AppDelegate : NSApplicationDelegate
 	{
-		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		[STAThread]
+		static void Main()
 		{
-			AppDomain.CurrentDomain.AssemblyResolve += (object sender, ResolveEventArgs a) =>  {
-				if (a.Name.StartsWith("MonoMac")) {
-					return typeof(MonoMac.AppKit.AppKitFramework).Assembly;
-				}
-				return null;
-			};
-			Program.RunGame();
+			using (var game = new MainGame())
+				game.Run();
 		}
-
-		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
-		{
-			return true;
-		}
-	}  
-	#endif
+	}
 }
 
---
+++ GridDominance.OpenGL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GridDominance.Shared;
+
+namespace GridDominance.OpenGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+                game.Run();
+        }
+    }
+}
