ef37866ceaca5b8047cae0cb6fb74cb0260f997a
Mike Schwörer <mailport@mikescher.de>
2016-02-14 02:31:14 +0100

    Begin Leveldesigner

---
+++ Source/Leveleditor/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Leveleditor
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.canvas = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.btnRefresh = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.canvas);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+			this.splitContainer1.Size = new System.Drawing.Size(1018, 839);
+			this.splitContainer1.SplitterDistance = 500;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// canvas
+			// 
+			this.canvas.BackColor = System.Drawing.Color.White;
+			this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.canvas.Location = new System.Drawing.Point(0, 0);
+			this.canvas.Name = "canvas";
+			this.canvas.Size = new System.Drawing.Size(1018, 500);
+			this.canvas.TabIndex = 0;
+			this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1018, 335);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Location = new System.Drawing.Point(3, 3);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(812, 329);
+			this.textBox1.TabIndex = 0;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.btnRefresh);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(821, 3);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(194, 329);
+			this.flowLayoutPanel1.TabIndex = 1;
+			// 
+			// btnRefresh
+			// 
+			this.btnRefresh.Location = new System.Drawing.Point(3, 3);
+			this.btnRefresh.Name = "btnRefresh";
+			this.btnRefresh.Size = new System.Drawing.Size(191, 23);
+			this.btnRefresh.TabIndex = 0;
+			this.btnRefresh.Text = "Refresh";
+			this.btnRefresh.UseVisualStyleBackColor = true;
+			this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1018, 839);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "MainForm";
+			this.Text = "LevelEditor";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Button btnRefresh;
+		private System.Windows.Forms.Panel canvas;
+	}
+}
+
---
+++ Source/Leveleditor/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Leveleditor
+{
+	public partial class MainForm : Form
+	{
+		readonly Bitmap graphicsBuffer = new Bitmap(1024, 640);
+
+		public MainForm()
+		{
+			RecreateBuffer();
+
+			InitializeComponent();
+		}
+
+		private void btnRefresh_Click(object sender, EventArgs e)
+		{
+			RecreateBuffer();
+			canvas.Invalidate();
+		}
+
+		private void RecreateBuffer()
+		{
+			using (Graphics g = Graphics.FromImage(graphicsBuffer))
+			{
+				g.SmoothingMode = SmoothingMode.AntiAlias;
+				g.Clear(Color.Black);
+
+				for (int x = 0; x < 8; x++)
+				{
+
+					g.DrawLine(Pens.DarkGray, x*128, 0, x*128, 640);
+				}
+				for (int y = 0; y < 5; y++)
+				{
+					g.DrawLine(Pens.DarkGray, 0, y * 128, 1024, y * 128);
+				}
+
+				g.FillEllipse(Brushes.Red, new Rectangle(10, 10, 32, 32));
+			}
+		}
+
+		private void canvas_Paint(object sender, PaintEventArgs e)
+		{
+			Rectangle dest;
+
+			Rectangle src = new Rectangle(0, 0, 1024, 640);
+			Rectangle disp = canvas.DisplayRectangle;
+			disp.Inflate(-10, -10);
+
+			if (src.Width*1.0/src.Height < disp.Width*1.0/ disp.Height)
+				dest = new Rectangle(0, 0, (src.Width * disp.Height) / src.Height, disp.Height);
+			else
+				dest = new Rectangle(0, 0, disp.Width, (src.Height * disp.Width )/ src.Width);
+
+			dest.X = (canvas.DisplayRectangle.Width - dest.Width)/2;
+			dest.Y = (canvas.DisplayRectangle.Height - dest.Height) /2;
+
+			e.Graphics.DrawImage(graphicsBuffer, dest, src, GraphicsUnit.Pixel);
+		}
+	}
+}
