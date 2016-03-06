a70dd359a823b5d8981473b4ea8a5824aa00f23c
Mike Schwörer <mailport@mikescher.de>
2016-03-06 01:03:14 +0100

    New Graphics - New Design

--- Source/GridDominance.Shared/Content/textures/fix_spritesheet_names.bat
+++ Source/GridDominance.Shared/Content/textures/fix_spritesheet_names.bat
@@ -1,4 +1,8 @@
-IF EXIST spritesheet-sheet.json (
-    DEL  spritesheet-sheet.json
-    MOVE spritesheet.json spritesheet-sheet.json
+IF EXIST spritesheet.json (
+    IF EXIST spritesheet-sheet.json (
+        DEL  spritesheet-sheet.json
+        MOVE spritesheet.json spritesheet-sheet.json
+    )
+
+    DEL  spritesheet.json
 )
