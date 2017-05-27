7acc0f341562184015138f0781150314fbd45bc1
Mike Schwörer <mailport@mikescher.de>
2017-05-27 18:36:03 +0200

    Android In-App-Billing (WIP)

--- Source/GridDominance.Android/Resources/Resource.designer.cs
+++ Source/GridDominance.Android/Resources/Resource.designer.cs
@@ -26,6 +26,7 @@ namespace GridDominance.Android
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.InAppBilling.Resource.String.library_name = global::GridDominance.Android.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -60,11 +61,27 @@ namespace GridDominance.Android
 			}
 		}
 		
-		public partial class Style
+		public partial class String
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Theme_Splash = 2130903040;
+			public const int library_name = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int Theme_Splash = 2130968576;
 			
 			static Style()
 			{
