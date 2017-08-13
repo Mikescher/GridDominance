8e56b17f78e890c16b7c92baeed15aec784fbbee
Mike Schwörer <mailport@mikescher.de>
2017-08-13 17:14:57 +0200

    release candidate 4

--- Source/GridDominance.Shared/Resources/GDConstants.cs
+++ Source/GridDominance.Shared/Resources/GDConstants.cs
@@ -4,7 +4,7 @@ namespace GridDominance.Shared.Resources
 {
 	public static class GDConstants
 	{
-		public static readonly Version Version = new Version(0,26,0,0);
+		public static readonly Version Version = new Version(0,27,0,0);
 		public static ulong IntVersion { get; } = (ulong)((((((Version.Major << 12) | Version.Minor) << 12) | Version.Build) << 12) | Version.Revision);
 
 		public const int TILE_WIDTH = 64;
