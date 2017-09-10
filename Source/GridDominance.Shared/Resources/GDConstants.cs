79643e4f1cec71a55a78d5b4977436023553b9bf
Mike Schwörer <mailport@mikescher.de>
2017-09-10 15:58:13 +0200

    [[ VERSION 1.0.5 ]]

--- Source/GridDominance.Shared/Resources/GDConstants.cs
+++ Source/GridDominance.Shared/Resources/GDConstants.cs
@@ -6,7 +6,7 @@ namespace GridDominance.Shared.Resources
 
 	public static class GDConstants
 	{
-		public static readonly Version Version = new Version(1,0,4,0);
+		public static readonly Version Version = new Version(1,0,5,0);
 		public static ulong IntVersion { get; } = (ulong)((((((Version.Major << 12) | Version.Minor) << 12) | Version.Build) << 12) | Version.Revision);
 
 		public const int TILE_WIDTH = 64;
