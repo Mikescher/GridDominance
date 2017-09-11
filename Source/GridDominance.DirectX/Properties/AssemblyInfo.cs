faa3f6197c97324d8e5fc202069ca375dfc77b9e
Mike Schwörer <mailport@mikescher.de>
2017-09-11 23:44:48 +0200

    [[ VERSION 1.0.6 ]]

---
+++ Source/GridDominance.DirectX/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GridDominance.DirectX")]
+[assembly: AssemblyProduct("GridDominance.DirectX")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70e1f3f9-50f5-4c73-a73d-4338e9df36f7")]
+
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
--- Source/GridDominance.iOS.Full/Properties/AssemblyInfo.cs
+++ Source/GridDominance.iOS.Full/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
