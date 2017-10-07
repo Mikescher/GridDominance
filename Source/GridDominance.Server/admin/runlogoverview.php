aa21f0e13125a555b95b22ad41f3ad99e2a9100f
Mike Schwörer <mailport@mikescher.de>
2017-10-07 18:47:32 +0200

    chart for /admin/runlogoverview

--- Source/GridDominance.Server/admin/runlogoverview.php
+++ Source/GridDominance.Server/admin/runlogoverview.php
@@ -13,6 +13,7 @@
 <body id="rootbox">
 
     <script src="jquery-3.1.0.min.js"></script>
+    <script src="Chart.min.js"></script>
 
     <h1><a href="index.php">Cannon Conquest | Admin Page</a></h1>
 
@@ -28,8 +29,121 @@
     <?php
 
 	$rloglist = getRunLogActionList();
+    $runlogs = [];
+	foreach ($rloglist as $raction) $runlogs[$raction['action']] = getRunLog($raction['action']);
+
+	function fmtd($df)
+    {
+		$d = DateTime::createFromFormat('Y-m-d H:i:s', $df);
+		$d->setTime(round($d->format('H')/6)*6, 0, 0);
+		return $d->format('Y-m-d H:i');
+    }
+
+	$dates = [];
+	foreach ($rloglist as $raction)
+	{
+		foreach ($runlogs[$raction['action']] as $entry) $dates []= fmtd($entry['exectime']);
+    }
+	asort($dates);
+	$dates = array_unique($dates);
+
+    $datedata = [];
+
+	foreach ($rloglist as $raction)
+	{
+	    $arr = [];
+
+	    $last = 0;
+	    foreach ($dates as $date)
+        {
+            $v = $last;
+            foreach ($runlogs[$raction['action']] as $entry)
+            {
+                if (fmtd($entry['exectime']) == $date) $v = round(($entry['duration']/$entry['count'])/(1000.0*1000.0), 5);
+            }
+			$arr []= $v;
+            $last = $v;
+        }
+
+		$datedata[$raction['action']] = $arr;
+    }
+
+    $COLORS =
+        [
+            '#4661EE','#EC5657','#1BCDD1','#8FAABB','#B08BEB','#3EA0DD','#F5A52A','#23BFAA','#FAA586','#EB8CC6',
+            '#9BC4E5','#04640D','#FEFB0A','#FB5514','#E115C0','#00587F','#0BC582','#FEB8C8','#9E8317',
+            '#01190F','#847D81','#58018B','#B70639','#703B01','#F7F1DF','#118B8A','#4AFEFA','#FCB164','#796EE6',
+            '#000D2C','#53495F','#F95475','#61FC03','#5D9608','#DE98FD','#98A088','#4F584E','#248AD0','#5C5300',
+            '#9F6551','#BCFEC6','#932C70','#2B1B04','#B5AFC4','#D4C67A','#AE7AA1','#C2A393','#0232FD','#6A3A35',
+            '#BA6801','#168E5C','#16C0D0','#C62100','#014347','#233809','#42083B','#82785D','#023087','#B7DAD2',
+            '#196956','#8C41BB','#ECEDFE','#2B2D32','#94C661','#F8907D','#895E6B','#788E95'
+        ];
 
     ?>
+
+    <div data-collapse>
+
+        <h2 class="open collapseheader">History</h2>
+        <div>
+            <canvas id="scoreChart1" width="85%" height="25%"></canvas>
+            <script>
+                let ctx1 = document.getElementById("scoreChart1").getContext('2d');
+
+                new Chart(ctx1,
+                    {
+                        type: 'line',
+                        data:
+                            {
+                                labels: [ <?php foreach ($dates as $rld) echo "'".$rld."',"; ?> ],
+                                datasets:
+                                    [
+										<?php $i=0; ?>
+										<?php foreach ($rloglist as $raction): ?>
+										<?php $i++; ?>
+										<?php if ($raction['action'] == 'cron') continue; ?>
+                                        {
+                                            label: '<?php echo $raction['action']; ?>',
+                                            data: [ <?php foreach ($datedata[$raction['action']] as $dd) echo $dd.","; ?> ],
+                                            borderColor: '<?php echo $COLORS[$i%count($COLORS)]; ?>',
+                                            backgroundColor: 'transparent',
+                                        },
+										<?php endforeach; ?>
+                                    ]
+                            },
+                    });
+            </script>
+        </div>
+    </div>
+
+    <div data-collapse>
+
+        <h2 class="collapseheader">History (Cron)</h2>
+        <div>
+            <canvas id="scoreChart2" width="85%" height="25%"></canvas>
+            <script>
+                let ctx2 = document.getElementById("scoreChart2").getContext('2d');
+
+                new Chart(ctx2,
+                    {
+                        type: 'line',
+                        data:
+                            {
+                                labels: [ <?php foreach ($dates as $rld) echo "'".$rld."',"; ?> ],
+                                datasets:
+                                    [
+										<?php foreach ($rloglist as $raction): ?>
+										<?php if ($raction['action'] != 'cron') continue; ?>
+                                        {
+                                            label: '<?php echo $raction['action']; ?>',
+                                            data: [ <?php foreach ($datedata[$raction['action']] as $dd) echo $dd.","; ?> ],
+                                        },
+										<?php endforeach; ?>
+                                    ]
+                            },
+                    });
+            </script>
+        </div>
+    </div>
 
     <?php foreach($rloglist as $raction): ?>
 
@@ -47,7 +161,7 @@
                 <th width="150px">Duration (Max)</th>
             </tr>
             </thead>
-			<?php foreach (getRunLog($raction['action']) as $entry): ?>
+			<?php foreach ($runlogs[$raction['action']] as $entry): ?>
                 <tr>
                     <td><?php echo $entry['exectime']; ?></td>
                     <td><?php echo $entry['min_timestamp'] .  " - " . $entry['max_timestamp']; ?></td>
