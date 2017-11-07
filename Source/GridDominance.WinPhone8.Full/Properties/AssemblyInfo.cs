374a565bb906d4c248897070b4e9aa2bdf2a38b2
Mike Schwörer <mailport@mikescher.de>
2017-11-07 19:16:23 +0100

    [[ VERSION 1.2.4 ]]

--- Source/GridDominance.WinPhone8.Full/Properties/AssemblyInfo.cs
+++ Source/GridDominance.WinPhone8.Full/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 [assembly: ComVisible(false)]
