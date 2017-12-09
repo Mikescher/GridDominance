41a75ab9a52ee335d468d0afe328ad4bddb39918
Mike Schwörer <mailport@mikescher.de>
2017-12-09 02:12:48 +0100

    update MonoSAM

--- Source/GridDominance.Shared/DeviceBridge/IGDOperatingSystemBridge.cs
+++ Source/GridDominance.Shared/DeviceBridge/IGDOperatingSystemBridge.cs
@@ -1,15 +1,10 @@
-﻿using MonoSAMFramework.Portable.GameMath.Geometry;
-using MonoSAMFramework.Portable.Network.Multiplayer;
+﻿using MonoSAMFramework.Portable.Network.Multiplayer;
 
 namespace MonoSAMFramework.Portable.DeviceBridge
 {
 	public interface IGDOperatingSystemBridge : ISAMOperatingSystemBridge
 	{
-		string FullDeviceInfoString { get; }
-		string DeviceName { get; }
-		string DeviceVersion { get; }
 		string AppType { get; }
-		FSize DeviceResolution { get; }
 
 		IBillingAdapter IAB { get; }
 
