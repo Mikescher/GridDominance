ff098bc1ab2e21a2b86b5b788d5025ce27283dc7
Mike Schwörer <mailport@mikescher.de>
2018-02-19 20:32:57 +0100

    StringUtils

---
+++ Language/StringUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoSAMFramework.Portable.Language
+{
+	public static class StringUtils
+	{
+		public static string BytesToString(Encoding enc, byte[] arr)
+		{
+			return enc.GetString(arr, 0, arr.Length);
+		}
+	}
+}
