05faec9a0150e5a2cd98957742fbc111c9fd8301
Mike Schwörer <mailport@mikescher.de>
2018-03-04 19:45:17 +0100

    added versionExtension

---
+++ Extensions/VersionExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoSAMFramework.Portable.Extensions
+{
+	public static class VersionExtension
+	{
+		public static ulong ToNum(this Version v)
+		{
+			ulong num = 0;
+			num |= (uint)v.Major;
+			num <<= 12;
+			num |= (uint)v.Minor;
+			num <<= 12;
+			num |= (uint)v.Build;
+			num <<= 12;
+			num |= (uint)v.Revision;
+
+			return num;
+		}
+	}
+}
