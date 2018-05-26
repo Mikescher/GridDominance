c57bcb7db2fae15dc67c24b5fe916718b8844f35
Mike Schwörer <mailport@mikescher.de>
2018-05-26 16:26:04 +0200

    EnsureNoError --> auto unpack AggregateException

--- Extensions/TaskExtension.cs
+++ Extensions/TaskExtension.cs
@@ -7,7 +7,14 @@ namespace MonoSAMFramework.Portable.Extensions
 	{
 		public static void EnsureNoError(this Task task)
 		{
-			task.ContinueWith(t => { SAMLog.Error("EnsureNoError", t.Exception); }, TaskContinuationOptions.OnlyOnFaulted);
+			task.ContinueWith(t =>
+			{
+				if (t.Exception != null && t.Exception.InnerExceptions.Count==1)
+					SAMLog.Error("EnsureNoError", t.Exception.InnerExceptions[0]);
+				else
+					SAMLog.Error("EnsureNoError", t.Exception);
+			}, 
+			TaskContinuationOptions.OnlyOnFaulted);
 		}
 
 		public static void RunAsync(this Task task)
