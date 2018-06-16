56c11fedfa7cb8bc399a42ff67358db70b0deb49
Mike Schwörer <mailport@mikescher.de>
2018-06-16 13:47:43 +0200

    gdapi index.php

---
+++ Source/GridDominance.Server/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<style type="text/css">
+		html { background-color: #CCC; height: 100%; }
+		body {  display: flex; justify-content: center; align-items: center; height: 100%; }
+		span { font-size: 20em; font-family: sans-serif; color: #333; text-shadow: 0 0 16px #000; }
+		@keyframes _ { 0% { transform: scale(1,1); } 50%   { transform: scale(1.1,1.1); } 100%   { transform: scale(1,1); } }
+		span { transform-origin: 50% 50%;}
+		span { animation-name: _; animation-duration: 6s; animation-timing-function: ease-in-out; animation-iteration-count: infinite; }
+	</style>
+</head>
+<body>
+<span>?</span>
+</body>
+</html>
